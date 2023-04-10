1a9df7febce6f3dd72fe9a270464634249542e54
Yara <yara.rodrigues.inacio@gmail.com>
2023-04-10 15:23:56 -0300

    cad - Ya

--- VE_FullBa/FullBodyAction/FullBodyAction/urls.py
+++ VE_FullBa/FullBodyAction/FullBodyAction/urls.py
@@ -4,4 +4,5 @@ from app_cadastro import views
 urlpatterns = [
     #fullbodyaction.com/cadastro/
     path('cadastro/', views.cadastro, name='cadastro'),
+    path('usuarios/', views.usuarios, name='listagem_usuarios')
 ]
--- VE_FullBa/FullBodyAction/app_cadastro/views.py
+++ VE_FullBa/FullBodyAction/app_cadastro/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from .models import Usuario
 
 def cadastro(request):
     return render(request,'fullbodyaction/cadastro.html')
+
+def usuarios(request):
+    # Salvar os dados da tela para o banco de dados
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.email = request.POST.get('email')
+    novo_usuario.save()
+
+    # Exibir todos os usuarios já cadastrados em uma nova página
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+
+    # Retornar os dados para a pagina de listagem de usuários 
+    return render(request, 'usuarios/usuarios.html', usuarios)
