61c6fae808576fcba042049ca4b605c0045069b9
Yara <yara.rodrigues.inacio@gmail.com>
2023-04-10 11:16:12 -0300

    y

--- myworld/proj_cadastro/app_cadastro/models.py
+++ myworld/proj_cadastro/app_cadastro/models.py
@@ -2,5 +2,3 @@ from django.db import models
 
 class Usuarios(models.Model):
     id_usuario = models.AutoField(primary_key=True)
-    nome = models.TextField(max_Length=255)
-    idade = models.IntegerField()
