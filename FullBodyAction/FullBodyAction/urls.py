7af9d34983c7c09eed53cecec64ba83a75b133a4
Erick <gets@cesar.school>
2023-04-15 16:30:24 -0300

    Ordem de prioridade do URL

--- FullBodyAction/FullBodyAction/urls.py
+++ FullBodyAction/FullBodyAction/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('home/', views.home, name= 'treino'),
     path('cadastro/', views.cadastro, name='usuarios'),
     path('login/', views.login, name='login'),
-    path('consumo_agua/', views.registro, name='registro_agua'),
     path('biceps/', views.musculo, name ='biceps'),
     path('costas/', views.musculo, name ='costas'),
     path('peito/', views.musculo, name ='peito'),
@@ -15,4 +14,5 @@ urlpatterns = [
     path('gluteos/', views.musculo, name ='gluteos'),
     path('posterior/', views.musculo, name ='posterior'),
     path('antebraco/', views.musculo, name ='antebraco'),
+    path('consumo_agua/', views.registro, name='registro_agua'),
 ]
