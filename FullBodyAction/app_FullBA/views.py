01e56caf584cefb327da051550e553398c20e412
Kaiquegb <kaiqueghsilva@gmail.com>
2023-04-15 11:44:03 -0300

    utilizei a função redirect da bibli shortcuts

--- FullBodyAction/app_FullBA/views.py
+++ FullBodyAction/app_FullBA/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .models import Usuario
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 def home(request):
     if request.method == "GET":
@@ -41,6 +42,29 @@ def treino(request):
         return render(request,'usuarios/treino.html')
     else:
         return HttpResponse('Deu zica')
+
+def treino(request):
+    if request.method == 'POST':
+        grupo_muscular = request.POST.get('grupo_muscular')
+        if grupo_muscular == 'Bíceps':
+            return redirect('biceps.html')
+        elif grupo_muscular =='Costas':
+            return redirect('costas.html')
+        elif grupo_muscular == 'Peito':
+            return redirect('peito.html')
+        elif grupo_muscular == 'Quadriceps':
+            return redirect('quadriceps.html')
+        elif grupo_muscular== 'Tríceps':
+            return redirect('triceps.html')
+        elif grupo_muscular == 'Panturrilhas':
+            return redirect('panturrilhas.html')
+        elif grupo_muscular == 'Glutéos':
+            return redirect('gluteos.html')
+        elif grupo_muscular == 'Posterior':
+            return redirect('posterior.html')
+        elif grupo_muscular == 'Antebraço':
+            return redirect('antebraco.html')
+
     
 
 @login_required    
@@ -54,6 +78,3 @@ def registro(request):
         return render(request,'usuarios/registro_agua.html')
     else:
         return HttpResponse('Algo deu errado :/')
-
-def bieceps(request):
-    return render(request, 'bíceps.html')
