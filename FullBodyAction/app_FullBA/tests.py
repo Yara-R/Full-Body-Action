80223ff363a64a285fd81283543cd53a15a5c218
Yara <yara.rodrigues.inacio@gmail.com>
2023-04-26 19:24:53 -0300

    selenium

--- FullBodyAction/app_FullBA/tests.py
+++ FullBodyAction/app_FullBA/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
 
 # Create your tests here.
+class TestHome(LiveServerTestCase):
+    browser = webdriver.Chrome()
+
+    def test_title(self):
+        self.browser.get('')
+        assert "The install worked" in self.browser.title
+
+    def test_link(self):
+        self.browser.get('')
