30f32db4864db8e7fa0467de5a5cdef9a227e762
Kaiquegb <kaiqueghsilva@gmail.com>
2023-04-27 16:27:33 -0300

    html e css da home colocadas

--- FullBodyAction/FullBodyAction/urls.py
+++ FullBodyAction/FullBodyAction/urls.py
@@ -3,7 +3,7 @@ from app_FullBA import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name = 'treino'),
+    path('', views.home, name = 'home'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.login, name='login'),
     path('musculo/', views.smash, name='smash'),
