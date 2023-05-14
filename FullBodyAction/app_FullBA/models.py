6c81ca68a4bbf231ad7ef4ac11ee360ec7b16359
Kaiquegb <kaiqueghsilva@gmail.com>
2023-05-14 18:25:52 -0300

    piedade pls

--- FullBodyAction/app_FullBA/models.py
+++ FullBodyAction/app_FullBA/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, User
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+from django.contrib.auth.models import AbstractUser
 
 class User(models.Model):
     name = models.CharField(max_length=50)
