678832abf1c6874430cf6c21e083a182846381c5
Yara <yara.rodrigues.inacio@gmail.com>
2023-06-10 17:08:23 -0300

    pff funciona :,(

--- app_FullBA/tests.py
+++ app_FullBA/tests.py
@@ -1,18 +1,26 @@
-import os
-import time
 from django.test import LiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+
+# Documentação selenium para Python: https://selenium-python.readthedocs.io/
+# Exemplos: https://ordinarycoders.com/blog/article/testing-django-selenium
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
-driver = webdriver.Chrome(options=chrome_options)
+browser = webdriver.Chrome(options=chrome_options)
 
+
+# Create your tests here.
 
 class TestHome(LiveServerTestCase):
     def test_title(self):
-        driver.get('http://127.0.0.1:8000/')
-        assert "The install worked" in driver.title
+        browser.get('http://127.0.0.1:8000/')
+        assert "The install worked" in browser.title
+
+    def test_href(self):
+        browser.get('http://127.0.0.1:8000/')
+        link = browser.find_element(By.CLASS_NAME, 'logo')
+        assert link.get_attribute('href') == 'https://www.djangoproject.com/'
 
